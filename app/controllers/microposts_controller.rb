44a3750356ccded1cfb3dae0c6170a424ef4b0da
besssho <paruttaboy.21@gmail.com>
2016-03-21 12:50:36 +0000

    review1

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,12 +7,12 @@ class MicropostsController < ApplicationController
        flash[:success] = "Micropost created!"
        redirect_to root_url
    else
-       render 'static_page/home'
+       render 'static_pages/home'
    end
  end
  
  def destroy
-  @micropost = current_user. microposts.find_by{id: params[:id]}
+  @micropost = current_user.microposts.find_by(id: params[:id])
   return redirect_to root_url if @micropost.nil?
   @micropost.destroy
   flash[:success] = "Micropost deleted"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update ,:followings,:followers]
   before_action :check_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :followings, :followers]
   
   def show
-    @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
@@ -35,14 +35,12 @@ class UsersController < ApplicationController
   
   def followings
     @title = "Followings"
-    @user = User.find(params[:id])
     @users = @user.followings
     render 'show_follow'
   end
   
   def followers
     @title = "Followers"
-    @user = User.find(params[:id])
     @users = @user.followers
     render 'show_follow'
   end
